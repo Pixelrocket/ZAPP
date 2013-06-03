c6a5884991e7e5b981a366b92c5ca71f40399f41
Wouter Scherphof <wouter.scherphof@gmail.com>
2013-06-03 15:14:26 +0200

    veregeten debug print statements verwijderd

--- src/app/rowcaption.lua
+++ src/app/rowcaption.lua
@@ -3,11 +3,6 @@ local function rowcaption (items, r, g, b)
     local row = event.row
     local item = items[row.id] or {text = nil}
     item.index = row.index -- needed as the prameter for TableViewWidget:deleteRow()
-    print("\n")
-    print(row.id)
-    for k,v in pairs(items) do
-      print(k,v)
-    end
     local text = item.text or "table row " .. row.index
     local rowtext = display.newText(row, text, 0, 0, native.systemFont, 14)
     rowtext.x = 10 + row.x - row.contentWidth / 2 + rowtext.contentWidth / 2
