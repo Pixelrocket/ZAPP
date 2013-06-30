49e874fb132f21c11ba3fa2dd4cf556cbc5c9a46
Wouter Scherphof <wouter.scherphof@gmail.com>
2013-06-30 07:50:01 +0200

    start met placeholders voor textfields

--- src/app/login.lua
+++ src/app/login.lua
@@ -6,24 +6,61 @@ local fields = {
   pwd = nil
 }
 
+local placeholders = {}
+
 local function input (event)
+  local textfield = event.target
   if event.phase == "began" then
-  elseif event.phase == "ended" then
   elseif event.phase == "ended" or event.phase == "submitted" then
+    local placeholder = placeholders[textfield]
+    placeholder.text = textfield.text
+    textfield.isVisible = false
+    placeholder.isVisible = true
+    placeholders[textfield] = nil
+    textfield:removeSelf()
   elseif event.phase == "editing" then
   end
+end
+
+local width = display.viewableContentWidth
+
+local function textfield (top, hint, returnKey)
+  local group = display.newGroup()
+
+  local line = display.newLine(group, 16,top + 44, 16,top + 48)
+  line:append(width - 16,top + 48, width - 16,top + 44)
+  line:setColor(153, 153, 153)
+
+  local text = display.newText(group, hint, 16 + 4, top + 4, width - 32 - 8, 40, "Roboto-Regular", 18)
+  text:setTextColor(153, 153, 153)
+
+  function text:touch (event)
+    if event.phase == "ended" then
+      textfield = native.newTextField(16, top + 4, width - 32, 40)
+      textfield.font = native.newFont("Roboto-Regular", 18)
+      textfield.hasBackground = false
+      textfield:setReturnKey(returnKey)
+      textfield:addEventListener("userInput", input)
+      native.setKeyboardFocus(textfield)
+      placeholders[textfield] = text
+      text.isVisible = false
+    end
+    return true
+  end 
+  text:addEventListener("touch", text)
+
+  return group
 end
 
 local login, group = EventEmitter:new(), display.newGroup()
 
 function login:init(top)
   if group.numChildren > 0 then return end
-  local width, height = display.viewableContentWidth, display.viewableContentHeight - top
+  local height = display.viewableContentHeight - top
   local bg = display.newRect(group, 0, top, width, height) bg:setFillColor(255, 255, 255)
   top = top + 16
-  fields.uid = native.newTextField(16, top + 4, width - 32, 40) fields.uid:addEventListener("userInput", input)
-  fields.uid.text = "Gebruikersnaam"
-  fields.uid:setReturnKey("next")
+  local uid = textfield(top, "Gebruikersnaam", "next")
+  group:insert(uid)
   top = top + 48
   fields.pwd = native.newTextField(16, top + 4, width - 32, 40) fields.pwd:addEventListener("userInput", input)
   fields.pwd.text = "Wachtwoord"
@@ -32,7 +69,7 @@ function login:init(top)
   local button = widget.newButton({
     label = "Inloggen",
     left = width - 16 - 96, top = top + 4, width = 96, height = 40,
-    font = "Roboto-Regular", fontSize = 14,
+    font = "Roboto-Regular", fontSize = 18,
     onRelease = function ()
       for k,v in pairs(fields) do
         print(k,v)
